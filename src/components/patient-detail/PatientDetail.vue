6c1eea22ea88a873581cb9566322e4ed707ecb97
daizhongchu <dzc314@163.com>
2021-11-03 19:45:40 +0800

    手术阶段文案及样式处理

--- src/components/patient-detail/PatientDetail.vue
+++ src/components/patient-detail/PatientDetail.vue
@@ -2,41 +2,67 @@
   <div class="patient-detail">
     <div class="patient-name">
       <div class="patient-name-left">
-        <span class="patient-pesonal-name">{{ name }}</span>
-        <span class="patient-pesonal-sex">{{ sex }}</span>
-        <span class="patient-pesonal-old">{{ age }}</span>
+        <span class="patient-pesonal-name">{{ option.name }}</span>
+        <span class="patient-pesonal-sex">{{ option.sex }}</span>
+        <span class="patient-pesonal-old">{{ option.age }}</span>
       </div>
-      <van-tag round class="opration-tag" type="primary"> 到手术间 </van-tag>
+      <van-tag round class="status" :class="[statusClass]" type="primary">
+        {{ $filters.status(option.status) }}
+      </van-tag>
     </div>
     <div class="patient-label">
       <span class="patient-label-title">手术-01间-01台</span>
-      <van-tag round class="opration-tag" type="primary">标签</van-tag>
+      <van-tag round class="opration-tag" type="primary">{{
+        option.type
+      }}</van-tag>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from 'vue';
 export default defineComponent({
   name: 'PatientDetail',
   props: {
-    name: {
-      type: String,
-      default: '未知',
+    option: {
+      type: Object,
+      default: () => ({
+        name: '四个名字',
+        sex: '男',
+        age: '99',
+        status: 16,
+        type: '急诊',
+      }),
     },
-    sex: {
-      type: String,
-      default: '未知',
-    },
-    age: {
-      type: String,
-      default: '未知',
+  },
+  computed: {
+    statusClass(): string {
+      const { status } = this.option;
+      if (!status) {
+        return '';
+      }
+      // 未开始
+      const style1: any[] = [2];
+      if (style1.includes(status)) {
+        return 'before';
+      }
+      // 进行中
+      const style2: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+      if (style2.includes(status)) {
+        return 'progress';
+      }
+      // 结束
+      const style3: any[] = [16, -99];
+      if (style3.includes(status)) {
+        return 'end';
+      }
+      return '';
     },
   },
   setup() {
-    return {}
+    return {};
   },
-})
+});
 </script>
 
 <style scoped></style>
