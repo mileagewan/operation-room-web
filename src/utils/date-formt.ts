16971b7cd7b8500bed665e0da08d98d3b93e5c82
EX-LIHONG011 <EX-LIHONG011@pingan.com.cn>
2021-11-04 14:56:29 +0800

    清除eslint报错

--- src/utils/date-formt.ts
+++ src/utils/date-formt.ts
@@ -6,7 +6,7 @@ import dayjs from 'dayjs'
 /* 
 *说明：传入'2021-10-11',返回'10月11日'
  */
-export function MonthDay(year: string): String {
+export function MonthDay(year: string): string {
   if (!year) return ''
   const month = dayjs(year).month()
   const day = dayjs(year).date()
