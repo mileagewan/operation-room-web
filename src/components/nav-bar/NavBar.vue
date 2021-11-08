017af626dd0f348587df71fa86fe698981e08429
daizhongchu <dzc314@163.com>
2021-11-08 11:29:04 +0800

    navbar优化，去掉无用代码

--- src/components/nav-bar/NavBar.vue
+++ src/components/nav-bar/NavBar.vue
@@ -3,15 +3,13 @@
     <van-nav-bar :title="titleBar" left-arrow @click-left="onClickLeft">
       <template #right>
         <component :is="rightComponent"></component>
-        <!-- <IconFont icon="icon-kuaijiesaoma" @click="openScanQRCode" /> -->
       </template>
     </van-nav-bar>
   </div>
 </template>
 
 <script lang="ts">
-import JsToFlutter from '@/utils/js-to-flutter';
-import { defineComponent, getCurrentInstance, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import ScanQrCode from './ScanQrCode.vue';
 export default defineComponent({
   name: 'NavBar',
@@ -33,23 +31,10 @@ export default defineComponent({
     const onClickLeft = (): void => {
       emit('goBack');
     };
-    // TODO 看是否有更好的方式取到全局注入的emitter类
-    const { appContext }: any = getCurrentInstance();
-    const emitter: any = appContext.config.globalProperties.emitter;
-    const openScanQRCode = () => {
-      // emitter.emit('scan-code-success', '扫码结果');
-      JsToFlutter.startScanQRCode().then((res) => {
-        // console.log(res);
-        // 扫码成功，emit事件
-        emitter.emit('scan-code-success', res);
-      });
-    };
 
     return {
-      emitter,
       titleBar,
       onClickLeft,
-      openScanQRCode,
     };
   },
 });
