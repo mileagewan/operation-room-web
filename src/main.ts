56300f14458bb84812d28061be5e0adcd2d4f01b
wanlicheng <>
2021-11-10 14:34:23 +0800

    【问题单号】：confirm 【修改说明】：项目开发

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,10 @@ import '@/assets/styles/index.scss';
 import Emitter from 'tiny-emitter';
 const emitter = new Emitter();
 
-// const log = new VConsole();
-// console.log(log)
+if (String(process.env.VUE_TEST_ENV) !== 'true') {
+  const log = new VConsole();
+  console.log(log)
+}
 
 const app = createApp(App);
 app.config.globalProperties.$filters = filters;
