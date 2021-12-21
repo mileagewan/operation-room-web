25186a3b2392ec52d86a0547668e3e75185bb6b2
wanlicheng <>
2021-12-21 16:04:49 +0800

    【问题单号】：清洁工已读信息 【修改说明】：清洁工已读信息

--- src/views/mobile/hooks/useMessageRead.ts
+++ src/views/mobile/hooks/useMessageRead.ts
@@ -2,17 +2,19 @@ import { onMounted } from 'vue'
 import Request from "@/service/request";
 import { ReturnData } from "@/types/interface-model";
 import { useRoute } from "vue-router";
+import JsToFlutter from "@/utils/js-to-flutter";
 function useMessageRead(): string {
   const route = useRoute();
   onMounted(() => {
     msgReadApi()
   })
   const msgReadApi = async () => {
-    if (route.query?.msgId && route.query?.type === "app") {
-      const params = `id=${route.query?.msgId}`
+    if (route.query?.msgid && route.query?.type === "app") {
+      const params = `id=${route.query?.msgid}`
       await Request.xhr('appMsgRead', {}, params).then((r: ReturnData) => {
         if (r.code === 200) {
           console.log('消息已读')
+          JsToFlutter.notifyFlutterRead(route.query.msgid as string);
         }
       })
     }
